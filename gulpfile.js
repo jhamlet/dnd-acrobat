12feb59047b1449ed5aa18256c09ccd4548854ec
Jerry Hamlet <jerry@hamletink.com>
2014-12-08 07:57:25 -0800

    Reworked for use with Rx

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,40 @@
-var gulp     = require('gulp'),
-    concat   = require('gulp-concat'),
-    browser  = require('gulp-browserify'),
-    uglify   = require('gulp-uglify'),
-    clean    = require('del'),
-    CLOBBER  = [];
+var gulp       = require('gulp'),
+    concat     = require('gulp-concat'),
+    browser    = require('gulp-browserify'),
+    uglify     = require('gulp-uglify'),
+    clean      = require('del'),
+    CLOBBER    = [];
     
 gulp.task('clobber', function (done) {
     clean(CLOBBER, done);
 });
+CLOBBER.push('pub');
 
-gulp.task('build', function () {
+gulp.task('ext', function () {
+    return gulp.
+        src('./lib/ext/index.js', { read: false }).
+        pipe(browser()).
+        pipe(uglify()).
+        pipe(concat('ext.js')).
+        pipe(gulp.dest('pub'));
+});
+
+gulp.task('rx', function () {
+    return gulp.
+        src('./lib/ext/rx.js', { read: false }).
+        pipe(browser()).
+        pipe(uglify()).
+        pipe(concat('ext.rx.js')).
+        pipe(gulp.dest('pub'));
+});
+
+gulp.task('character', function () {
     return gulp.src('./lib/index.js').
         pipe(browser()).
-        pipe(concat('dnd-acrobat.js')).
-        pipe(uglify()).
-        pipe(gulp.dest('./'));
+        pipe(concat('character.js')).
+        pipe(gulp.dest('pub'));
 });
-CLOBBER.push('./pub/dnd-acrobat.js');
 
+gulp.task('build', ['ext', 'rx', 'character']);
 gulp.task('default', ['build']);
 
